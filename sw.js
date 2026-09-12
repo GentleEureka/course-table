ddfd041e966916e3dd3738da9271151d61c38874
七世温柔 <126140849+GentleEureka@users.noreply.github.com>
2026-09-12 23:43:55 +0800

    Bump PWA cache for v7 section controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kejian-pwa-v7-1';
+const CACHE_NAME = 'kejian-pwa-v7-2';
 const APP_SHELL = ['./','./index.html','./style.css','./theme.css','./course-density.css','./importers.js','./appearance.js','./app.js','./v5.js','./v6.js','./v7.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 const ASSET_DB='kejian-assets-v1', ASSET_STORE='assets';
 const CUSTOM_ICONS={
