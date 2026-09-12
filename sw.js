dfd260ae3b0cda88fa7078bfbb7c4926f38c2980
七世温柔 <126140849+GentleUlrica@users.noreply.github.com>
2026-09-12 20:08:49 +0800

    v5: refresh PWA cache for sidebar About layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kejian-pwa-v5-4';
+const CACHE_NAME = 'kejian-pwa-v5-5';
 const APP_SHELL = ['./','./index.html','./style.css','./theme.css','./importers.js','./appearance.js','./app.js','./v5.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 const ASSET_DB='kejian-assets-v1', ASSET_STORE='assets';
 const CUSTOM_ICONS={
